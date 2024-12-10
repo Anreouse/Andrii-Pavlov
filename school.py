9a9c0cb934850a966ba8ed7cf34a83aab74c1277
Andrii PAVLOV <158120703+Anreouse@users.noreply.github.com>
2024-12-10 20:34:50 +0200

    Delete school.py

--- school.py
+++
@@ -1,9 +0,0 @@
-import string
-
-input_str = input("Введіть два символи через дефіс: ")
-
-start_index, end_index = [string.ascii_letters.index(c) for c in input_str.split('-')]
-
-result = string.ascii_letters[start_index:end_index + 1]
-
-print("Результат:", result)
