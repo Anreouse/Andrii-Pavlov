7842e3e798449f913a1314952dca6094ad6a9eb8
Andrii PAVLOV <158120703+Anreouse@users.noreply.github.com>
2024-12-14 20:51:37 +0200

    Delete Homework.py

--- Homework.py
+++
@@ -1,9 +0,0 @@
-def common_elements():
-    element_3 = {x for x in range(100) if x % 3 == 0}
-    element_5 = {x for x in range(100) if x % 5 == 0}
-
-    intersection = element_3 & element_5
-
-    return intersection
-result = common_elements()
-print(result)
